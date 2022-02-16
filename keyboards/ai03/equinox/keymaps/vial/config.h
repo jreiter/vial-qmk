d29a715258ce147666d5a6dd9982fb2478521a66
jreiter <johnreiter@gmail.com>
2022-02-16 11:28:08 -0500

    Add equinox vial keymap

---
+++ keyboards/ai03/equinox/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x47, 0x45, 0x0B, 0x02, 0xC4, 0xA8, 0x1C, 0x99}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 1 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
